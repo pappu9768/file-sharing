4d6300ef883b82f46d3495fc7dc82c4371fbfc3e
pappu9768 <9930pawanshah@gmail.com>
2026-09-25 10:47:27 +0530

    adding .env file in client

--- client/src/api/callApi.ts
+++ client/src/api/callApi.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:5000'
+    // baseURL:'http://localhost:5000'
+    baseURL: import.meta.env.VITE_API_URL
 })
 // axios.interceptors.request.use((config) => {
     
